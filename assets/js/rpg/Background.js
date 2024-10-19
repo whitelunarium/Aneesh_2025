d8d18c108f3e61bb79fb51753a8c5a3a44383b9c
Aneesh Deevi <140877611+whitelunarium@users.noreply.github.com>
2024-10-19 12:47:00 -0700

    Update Background.js

--- assets/js/rpg/Background.js
+++ assets/js/rpg/Background.js
@@ -1,32 +1,43 @@
 import GameEnv from './GameEnv.js';
 
-export class Background {
-    constructor(imageSrc = null) {
-        if (imageSrc) {
-            this.image = new Image();
-            this.image.src = imageSrc.src;
-        } else {
-            this.image = null;
+class Background {
+    constructor(images = []) {
+        this.images = images.map(src => {
+            const img = new Image();
+            img.src = src;
+            return img;
+        });
+        this.currentImageIndex = 0;
+        this.loadImages();
+    }
+
+    loadImages() {
+        const promises = this.images.map(img => {
+            return new Promise((resolve, reject) => {
+                img.onload = resolve;
+                img.onerror = reject;
+            });
+        });
+
+        Promise.all(promises).then(() => {
+            console.log('All background images loaded successfully.');
+        }).catch(() => {
+            console.error('Failed to load some background images.');
+        });
+    }
+
+    draw() {
+        const currentImage = this.images[this.currentImageIndex];
+        if (currentImage) {
+            GameEnv.ctx.drawImage(
+                currentImage,
+                0, 0, GameEnv.innerWidth, GameEnv.innerHeight
+            );
         }
     }
 
-    /* To draws are used to capture primary frame and wrap around to next frame
-     * x to y is primary draw
-     * x + width to y is wrap around draw
-    */
-    draw() {
-        const ctx = GameEnv.ctx;
-        const width = GameEnv.innerWidth;
-        const height = GameEnv.innerHeight;
-
-        if (this.image) {
-            // Draw the background image scaled to the canvas size
-            ctx.drawImage(this.image, 0, 0, width, height);
-        } else {
-            // Fill the canvas with white if no background image is provided
-            ctx.fillStyle = '#87CEEB';
-            ctx.fillRect(0, 0, width, height);
-        }
+    changeBackground() {
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
     }
 }
 
