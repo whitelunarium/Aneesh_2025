b8daa1b6f721c1294b9db2a9c6484bb2ab5454b4
Aneesh Deevi <140877611+whitelunarium@users.noreply.github.com>
2024-10-19 12:47:24 -0700

    Update GameControl.js

--- assets/js/rpg/GameControl.js
+++ assets/js/rpg/GameControl.js
@@ -1,46 +1,31 @@
 import GameEnv from './GameEnv.js';
-import Background from './Background.js';
 import Player from './Player.js';
+import Fish from './Fish.js';
 
-/**
- * The GameControl object manages the game.
- * 
- * This code uses the JavaScript "object literal pattern" which is nice for centralizing control logic.
- * 
- * The object literal pattern is a simple way to create singleton objects in JavaScript.
- * It allows for easy grouping of related functions and properties, making the code more organized and readable.
- * In the context of GameControl, this pattern helps centralize the game's control logic, 
- * making it easier to manage game states, handle events, and maintain the overall flow of the game.
- * 
- * @type {Object}
- * @property {Player} player - The player object.
- * @property {function} start - Initialize game assets and start the game loop.
- * @property {function} gameLoop - The game loop.
- * @property {function} resize - Resize the canvas and player object when the window is resized.
- */
-const GameControl = {
-
-    start: function(assets = {}) {
-        GameEnv.create(); // Create the Game World, this is pre-requisite for all game objects.
-        this.background = new Background(assets.image || null);
-        this.player = new Player(assets.sprite || null);
-        this.gameLoop();
-    },
-
-    gameLoop: function() {
-        GameEnv.clear(); // Clear the canvas
-        this.background.draw();
-        this.player.update();
-        requestAnimationFrame(this.gameLoop.bind(this));
-    },
-
-    resize: function() {
-        GameEnv.resize(); // Adapts the canvas to the new window size, ie a new Game World.
-        this.player.resize();
+class GameControl {
+    constructor() {
+        // Initialization code if needed
     }
-};
 
-// Detect window resize events and call the resize function.
-window.addEventListener('resize', GameControl.resize.bind(GameControl));
+    start(assets = {}) {
+        GameEnv.create(); // Create the Game World
+        this.player = new Player(assets.sprite || null);
+        this.fish = new Fish(assets.sprite2 || null);
+        this.gameLoop();
+    }
+
+    gameLoop() {
+        GameEnv.clear(); // Clear the canvas
+        this.player.update(); // Update the player
+        this.fish.update(); // Update the fish
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
+
+    resize() {
+        GameEnv.resize(); // Resize the canvas and player
+        this.player.resize();
+        this.fish.resize();
+    }
+}
 
 export default GameControl;
